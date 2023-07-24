b6c5f5143ab24c97476da4f86cf8ed148e103bb5
z3phyron-snides <z3phyron-snides@gmail.com>
2023-07-24 09:17:01 -0700

    app(chore): corections

--- utils/helpers/toTitleCase.ts
+++
@@ -1,7 +0,0 @@
-export default function toTitleCase(str: string) {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
