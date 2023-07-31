9a75eeb3ee055c0c8678bb380c00854a38603416
z3phyron-snides <z3phyron-snides@gmail.com>
2023-07-31 16:45:03 -0700

    app(chore)

--- app/api/order/subscription/route.ts
+++ app/api/order/subscription/route.ts
@@ -1,34 +1,34 @@
-import { closeDB, connectDB } from "@/utils/db";
-import { authOptions } from "@/utils/helpers/authOptions";
-import { Subscription } from "@/utils/models/Subscription";
-import { Order } from "@/utils/models/Order";
-import User from "@/utils/models/Users";
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+// import { closeDB, connectDB } from "@/utils/db";
+// import { authOptions } from "@/utils/helpers/authOptions";
+// import { Subscription } from "@/utils/models/Subscription";
+// import { Order } from "@/utils/models/Order";
+// import User from "@/utils/models/Users";
+// import { getServerSession } from "next-auth";
+// import { NextResponse } from "next/server";
 
 
-export async function GET(req: Request, res: Response) {
-  try {
-    await connectDB();
+// export async function GET(req: Request, res: Response) {
+//   try {
+//     await connectDB();
 
     
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return NextResponse.json({ message: "you are not loggedIn" });
-    }
+//     const session = await getServerSession(authOptions);
+//     if (!session) {
+//       return NextResponse.json({ message: "you are not loggedIn" });
+//     }
 
-    const user = await User.findById(session.user._id);
-    if (!user) {
-      return NextResponse.json({ message: "user does not exist" });
-    }
+//     const user = await User.findById(session.user._id);
+//     if (!user) {
+//       return NextResponse.json({ message: "user does not exist" });
+//     }
 
-    const ordersHistory = await Order.find({ user });
+//     const ordersHistory = await Order.find({ user });
 
-    return NextResponse.json({ ordersHistory, success: true }, { status: 201 });
-  } catch (err) {
-    console.error(err);
-    NextResponse.json({ error: "An error occurred" }, { status: 500 });
-  } finally {
-    await closeDB();
-  }
-}
+//     return NextResponse.json({ ordersHistory, success: true }, { status: 201 });
+//   } catch (err) {
+//     console.error(err);
+//     NextResponse.json({ error: "An error occurred" }, { status: 500 });
+//   } finally {
+//     await closeDB();
+//   }
+// }
