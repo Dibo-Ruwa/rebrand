4554114fc78a63aa7c750abdeae5ed205785b562
z3phyron-snides <z3phyron-snides@gmail.com>
2023-08-12 08:34:15 -0700

    app(chore):

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,17 @@
 import DashboardRoutes from "@/component/DasboardRoutes/DashBoardRoutes";
-import AdminRoutes from "@/component/adminRoutes/AdminRoutes";
-import { authOptions } from "@/utils/helpers/authOptions";
-import { getServerSession } from "next-auth";
+// import AdminRoutes from "@/component/adminRoutes/AdminRoutes";
+// import { authOptions } from "@/utils/helpers/authOptions";
+// import { getServerSession } from "next-auth";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session: any = getServerSession(authOptions);
-
+  
   return (
     <section>
-      {session?.user?.role === "user" ? <DashboardRoutes /> : <AdminRoutes />}
+       <DashboardRoutes /> 
 
       {children}
     </section>
